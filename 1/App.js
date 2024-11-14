0edebf55fc27c1656cf3a5efbad21611d690d63d
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-11-14 23:41:50 +0530

    Done with the Namaste world and moving forward to revising the javascript to understand the node js in better manner

---
+++ 1/App.js
@@ -0,0 +1,13 @@
+let Course_Name = "Namaste NodeJs ";
+let Teacher_name = "Akshay Saini";
+
+
+console.log("I am Learning Node js from ", Course_Name, " Course ");
+console.log("The instructor for is course is great great ", Teacher_name,  " .");
+
+
+setTimeout(()=> {
+    console.log("Hello world ");
+}, 2000)
+
+console.log(global);
